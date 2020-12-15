5698485e77c4620d92cbebf3863b825ef0a9218f
hodorsec <hodorsec@protonmail.com>
2020-12-15 10:19:56 +0100

    Added PostgreSQL UDF C Reverse Shell

---
+++ Revshell/Windows/c_postgres_udf/build_dll.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set PROGRAM=%1
+set BASENAME=%PROGRAM:~0,-2%
+
+@echo on
+
+for %%a in (x86 amd64) do (
+    setlocal
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %%a
+    csc /target:module empty.cs
+    cl /c %PROGRAM%
+    link /DLL /LTCG /CLRIMAGETYPE:IJW /out:%BASENAME%_%%a.dll %BASENAME%.obj empty.netmodule
+    del %BASENAME%.obj empty.netmodule
+    endlocal
+)
+
